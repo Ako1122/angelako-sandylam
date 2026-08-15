9e250208194bb84282d075b0bc22788e444cad4c
Ako1122 <164748341+Ako1122@users.noreply.github.com>
2026-08-16 03:06:26 +0800

    排除都市心封面在干擾答案中

--- sandylam-netlify/js/guess-cover-version-album.js
+++ sandylam-netlify/js/guess-cover-version-album.js
@@ -6,7 +6,7 @@ const BLOCKED_DISTRACTOR_COVERS = new Set(
   [
     "08.jpg","18.jpg","19.jpg","20.jpg","21.jpg","23.jpg","27.jpg","28.jpg",
     "30.jpg","31.jpg","32.jpg","33.webp","34.jpg","35.jpg","36.png","37.webp",
-    "38.jpg","43.jpg","44.jpg","46.jpg","45.jpg","47.jpg","49.jpg","50.jpg",
+    "38.jpg","41.jpg","43.jpg","44.jpg","46.jpg","45.jpg","47.jpg","49.jpg","50.jpg",
     "51.jpg","52.jpg","53.webp","54.jpg","55.jpg","56.jpg","57.png","58.webp",
     "59.jpg","60.jpg","61.png","62.jpg","63.jpg","64.jpg","65.jpg","66.jpg",
     "67.jpg","68.png","69.jpg","70.webp","73.webp","74.jpg","76.jpg","77.webp",
